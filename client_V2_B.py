b73790314ed96c82563c9c1e008433c6d072a274
komiko <3213900199@qq.com>
2024-11-26 11:38:37 +0800

    B_client.py

--- client_V2_B.py
+++ client_V2_B.py
@@ -1,19 +1,21 @@
 from pwn import *
 import logging
-import hashlib
-import hmac
-import os
 from crypto import *
+import json 
 
 # 配置日志
 logging.basicConfig(filename='client_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
 
-# 生成客户端的私钥和公钥
-client_private_key,client_public_key = get_key()
 
 # 连接服务器
 client = remote('localhost', 12345)
 logging.info('Connected to server')
+
+"""
+Digital Signature
+"""
+# 生成客户端的私钥和公钥
+client_private_key,client_public_key = get_key()
 
 # 接收服务器公钥
 server_public_key = client.recv(32)
@@ -23,38 +25,75 @@ logging.info(f'Received server public key: {server_public_key.hex()}')
 client.send(client_public_key)
 logging.info(f'Sent client public key: {client_public_key.hex()}')
 
+#client hello
+client_hello = b'hello server'
+client.send(client_hello)
+signature = sign_message(client_hello,client_private_key)
+client.send(signature)
+logging.info(f'Sent client hello')
+
+#验签
+rcv = client.recv(1024).decode()
+is_valid =VerifySignature(rcv, signature, client_public_key)
+if is_valid:
+    print("Signature is valid.")
+else:
+    print("Signature is invalid.")
+logging.info(f'Signature ')
+
+
 #接收DH参数
-p, g = client.recv()
-logging.info(f'Received p,g is: {p.hex()}{g.hex()}')
+p = int(client.recv(1024).decode())  # 接收并转换 p
+logging.info(f"The server has sent DH parameter p: {p}")
+g = int(client.recv(1024).decode())  # 接收并转换 g
+logging.info(f"The server has sent DH parameter g: {g}")
+
 
 #生成客户端协商私钥公钥
 client_dh_key = generate_private_key()
-client_exchange_key =  exchange_key()
+client_exchange_key =  exchange_key(client_dh_key)
 
 #发送客户端协商公钥
-client.send(client_exchange_key)
+client.send(str(client_exchange_key).encode())
 logging.info((f'Sent client_exchange_key{client_exchange_key}'))
 
 #接受服务端协商公钥
-server_exchange_key = client.recv()
+server_exchange_key = int(client.recv(1024).decode())
+logging.info(f"Receive the shared public key from the server: {server_exchange_key}")
 
 # 生成共享密钥
 shared_secret = shared_key(client_dh_key,server_exchange_key)
-logging.info(f'Generated shared secret: {shared_secret.hex()}')
+print(shared_secret)
+logging.info(f'Generated shared secret: {shared_secret}')
 
 # 发送消息
-message = b'Hello from client'
-encrypted_message = hmac.new(shared_secret, message, hashlib.sha256).digest()
-client.send(encrypted_message)
-logging.info(f'Sent encrypted message: {encrypted_message.hex()}')
+message = b'Dday landing month 6 month 6 year 1944'
+encrypted_message = process_message(shared_secret,message.decode())[0]
+client.send(json.dumps(encrypted_message).encode())
+logging.info(f"send encrypted_message: {encrypted_message}")
+
+#发送原消息hash值
+m_hash = sha256(message)
+client.send(json.dumps(m_hash).encode())
+logging.info(f'Send the hash value of the original message:{m_hash}')
 
 # 接收响应
-encrypted_response = client.recv(1024)
-logging.info(f'Received encrypted response: {encrypted_response.hex()}')
+encrypted_message = json.loads(client.recv(4096).decode())
+logging.info(f"Receive encrypted messages: {encrypted_message}")
 
 # 解密响应
-decrypted_response = hmac.new(shared_secret, encrypted_response, hashlib.sha256).digest()
-logging.info(f'Decrypted response: {decrypted_response}' )
+decrypted_message = process_message(shared_secret,encrypted_message)[1]
+print(decrypted_message)
+logging.info(f"The decrypted message is: {decrypted_message}")
+
+#验证消息完整性
+#接收原消息hash值
+m_hash_1 = json.loads(client.recv(4096).decode())
+m_hash_2 =  sha256(decrypted_message)
+if m_hash_1 == m_hash_2:
+    print("Success: SHA-256 hash values match!")
+else:
+    print("Error: SHA-256 hash values do not match!")
 
 client.close()
 logging.info('Connection closed')
